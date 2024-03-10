28feb62cf8b398a1d4ab51b6756dc80c0d6626c6
PoorlyDefinedBehaviour <brunotj2015@hotmail.com>
2024-03-10 16:23:01 -0300

    creating scopes using with

---
+++ chapter_4/with-scope-fmt.exs
@@ -0,0 +1,15 @@
+content = "Now is the time"
+
+lp =
+  with {:ok, file} = File.open("/etc/passwd"),
+       content = IO.read(file, :all),
+       :ok = File.close(file),
+       [_, uid, gid] = Regex.run(~r/^_lp:.*?:(\d+):(\d+)/m, content) do
+    "Group: #{gid}, User: #{uid}"
+  end
+
+IO.puts(lp)
+# Group: 26, User: 26
+
+IO.puts(content)
+# Now is the time
